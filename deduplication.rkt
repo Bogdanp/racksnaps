a61cb7ed5cf29dcf25b9fc9271c6bb6055345181
Bogdan Popa <bogdan@defn.io>
2020-05-04 12:44:41 +0300

    deduplication: fix hashing of zip files
    Closes #2

--- deduplication.rkt
+++ deduplication.rkt
@@ -1,14 +1,43 @@
 #lang racket/base
 
-(require file/sha1
+(require file/unzip
+         openssl/sha1
          racket/file
          racket/match
-         racket/path)
+         racket/path
+         racket/port)
 
 (provide
  dedupe-snapshot)
 
 (define-logger deduper)
+
+;; Zip files contain the modification times of each file contained
+;; within them so hashing them does not produce deterministic values.
+;; Instead, we must sort the files by name and then hash the contents
+;; of the files within the zips themselves, which is what this
+;; function does.
+(define (zip-digest path)
+  (call-with-unzip path
+    (lambda (temp-path)
+      (define-values (in out)
+        (make-pipe))
+
+      (define sorted-paths
+        (sort (find-files
+               file-exists?
+               temp-path)
+              path<?))
+
+      (thread
+       (lambda ()
+         (for ([p (in-list sorted-paths)])
+           (call-with-input-file p
+             (lambda (f-in)
+               (copy-port f-in out))))
+         (close-output-port out)))
+
+      (sha1 in))))
 
 (define (dedupe-snapshot snapshot-path store-path)
   (define all-pkg-archives
@@ -20,7 +49,7 @@
     (when (link-exists? path)
       (error 'dedupe-snapshot "path ~a is already a link" path))
 
-    (define digest (call-with-input-file path sha1))
+    (define digest (zip-digest path))
     (match-define (list _ d1 d2 fn)
       (regexp-match #rx"(..)(..)(.+)" digest))
 
