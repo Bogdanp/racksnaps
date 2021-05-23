2edfa4a5f7703af1be3aff55b015c25406e5c4e3
Bogdan Popa <bogdan@defn.io>
2021-05-23 11:26:19 +0300

    build-snapshot: build packages sequentially

--- built-snapshot.rkt
+++ built-snapshot.rkt
@@ -19,9 +19,6 @@
 (define-logger setup)
 (define stop-logger (start-logger '(build common deduper docker pkg setup)))
 
-(define current-concurrency
-  (make-parameter (processor-count)))
-
 (define docker
   (find-executable-path "docker"))
 
@@ -47,7 +44,7 @@
          raco pkg config --set catalogs \
            file://@|built-snapshot-path|/catalog/ \
            file://@|snapshot-path|/catalog/
-         racket -M -l- raco pkg install --batch --auto --fail-fast --no-docs -j 2 @name
+         racket -M -l- raco pkg install --batch --auto --fail-fast --no-docs @name
          racket -M -l- raco pkg create --built --dest @|built-snapshot-path|/pkgs --from-install @name
          }))
 
@@ -94,44 +91,33 @@
   (make-directory* (build-path built-catalog-path "pkg"))
   (make-directory* built-pkgs-path)
 
-  (define sema (make-semaphore (current-concurrency)))
-  (define ch (make-async-channel (* (current-concurrency) 8)))
-  (for ([name (in-list names)])
-    (thread
-     (lambda ()
-       (call-with-semaphore sema
-         (lambda ()
-           (with-handlers ([exn:fail?
-                            (lambda (e)
-                              (log-build-error "failed to build ~a~n error: ~a" name (exn-message e))
-                              (async-channel-put ch (list 'failed name)))])
-             (define built?
-               (build-package root-path snapshot-path built-snapshot-path name))
-
-             (async-channel-put ch (if built?
-                                       (list 'built name)
-                                       (list 'failed name)))))))))
-
   (define total-pkgs (length names))
   (define pkgs-all
     (for/fold ([pkgs-all (hash)])
-              ([i (in-range total-pkgs)])
-      (log-build-info "progress: [~a/~a]" i total-pkgs)
-      (match (sync ch)
-        [(list 'built name)
-         (define info (call-with-input-file (build-path snapshot-path "catalog" "pkg" name) read))
-         (define new-checksum (file->string (build-path built-pkgs-path (format "~a.zip.CHECKSUM" name))))
-         (define new-info
-           (~> info
-               (hash-set 'checksum new-checksum)
-               (hash-set 'versions (hasheq 'default (hasheq 'source ('source info)
-                                                            'checksum new-checksum)))))
-         (write/rktd (build-path built-catalog-path "pkg" name) new-info)
-         (hash-set pkgs-all name new-info)]
+              ([name (in-list names)]
+               [i (in-naturals 1)])
+      (with-handlers ([exn:fail?
+                       (lambda (e)
+                         (begin0 pkgs-all
+                           (log-build-error "failed to build ~a~n error: ~a" name (exn-message e))))])
+        (define built?
+          (build-package root-path snapshot-path built-snapshot-path name))
+        (log-build-info "progress: [~a/~a]" i total-pkgs)
+        (cond
+          [built?
+           (define info (call-with-input-file (build-path snapshot-path "catalog" "pkg" name) read))
+           (define new-checksum (file->string (build-path built-pkgs-path (format "~a.zip.CHECKSUM" name))))
+           (define new-info
+             (~> info
+                 (hash-set 'checksum new-checksum)
+                 (hash-set 'versions (hasheq 'default (hasheq 'source ('source info)
+                                                              'checksum new-checksum)))))
+           (write/rktd (build-path built-catalog-path "pkg" name) new-info)
+           (hash-set pkgs-all name new-info)]
 
-        [(list 'failed name)
-         (begin0 pkgs-all
-           (log-build-warning "failed to build ~a" name))])))
+          [else
+           (begin0 pkgs-all
+             (log-build-warning "failed to build ~a" name))]))))
 
   (write/rktd (build-path built-catalog-path "pkgs") (sort (hash-keys pkgs-all) string<?))
   (write/rktd (build-path built-catalog-path "pkgs-all") pkgs-all))
@@ -176,16 +162,11 @@
 (module+ main
   (require racket/cmdline)
   (command-line
-   #:once-each
-   [("-c" "--concurrency")
-    concurrency
-    "the maximum number of packages to archive at once"
-    (current-concurrency (string->number concurrency))]
    #:args (root-path snapshot-path built-snapshot-path store-path . pkgs)
    (file-stream-buffer-mode (current-output-port) 'line)
    (parameterize ([current-pkg-catalogs (list (path->url (build-path snapshot-path "catalog")))])
      (define packages-to-build (if (null? pkgs) (get-all-pkg-names/topological) pkgs))
-     (log-build-info "about to build ~a packages with ~a concurrency" (length packages-to-build) (current-concurrency))
+     (log-build-info "about to build ~a packages" (length packages-to-build))
      (build-packages root-path snapshot-path built-snapshot-path packages-to-build)
      (dedupe-snapshot built-snapshot-path store-path)
      (fix-catalog-checksums built-snapshot-path)
